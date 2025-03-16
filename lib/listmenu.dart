0665e30ba866f57e32b6839fb51fd88515bb18d4
LLWY <112401932+UrrOis@users.noreply.github.com>
2025-03-16 10:32:41 +0700

    Add lib/listmenu.dart 1742095961171

---
+++ lib/listmenu.dart
@@ -0,0 +1,15 @@
+class Menu {
+  final String name;
+  final int price;
+
+  Menu(this.name, this.price);
+}
+
+List<Menu> menuList = [
+  Menu('Nasi Goreng', 13000),
+  Menu('Mie Ayam', 12000),
+  Menu('Sate Ayam', 15000),
+  Menu('Bakso', 12000),
+  Menu('Soto Ayam', 13000),
+  Menu('Gado-Gado', 10400),
+];
